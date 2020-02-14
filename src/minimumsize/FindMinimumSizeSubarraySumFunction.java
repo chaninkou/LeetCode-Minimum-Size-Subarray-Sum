c88bc80fbba11f057fc66d2cc419e65dd5f3dddf
chaninkou <michaelkoulove@yahoo.com>
2020-02-14 00:25:17 -0800

    Great review on sliding windows with O(N), moving two pointers.

--- src/minimumsize/FindMinimumSizeSubarraySumFunction.java
+++ src/minimumsize/FindMinimumSizeSubarraySumFunction.java
@@ -1,6 +1,7 @@
 package minimumsize;
 
 public class FindMinimumSizeSubarraySumFunction {
+	// Sliding window methods, o(N) since end and from pointers will move n times = O(N + N) worst case 
 	public int minSubArrayLen(int s, int[] nums) {
 		if (nums == null || nums.length == 0) {
 			return 0;
@@ -16,11 +17,12 @@ public class FindMinimumSizeSubarraySumFunction {
 		// We do max_value since any number will be the smallest
 		int min = Integer.MAX_VALUE;
 
+		// move end pointer until we find a sum of s or greater
 		while (end < nums.length) {
-			// Keep adding until the sum is s or greater
 			sum += nums[end];
 			end++;
 
+			// Once we find it, get the length and compare to the min, move the from pointer now
 			while (sum >= s) {
 				min = Math.min(min, end - from);
 
@@ -30,6 +32,7 @@ public class FindMinimumSizeSubarraySumFunction {
 		}
 
 		// If min == max_value, which mean the sum don't add up the the target
+		// edge case if min does add up to max_value, use a boolean instead
 		if (min == Integer.MAX_VALUE) {
 			return 0;
 		} else {
